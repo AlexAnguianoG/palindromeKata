efd31e6138e7461705f26f2f6d09985f446e6542
Alejandro Aguiano <alex1203@live.com.mx>
2021-04-16 16:59:08 -0600

    Step One

---
+++ src/main/java/mx/tec/kata/PalindromeValidator.java
@@ -0,0 +1,12 @@
+package mx.tec.kata;
+
+public class PalindromeValidator {
+	
+	public boolean validate (String str) {
+		if(str.length() == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
